0ab155ec739d425e7310f01ceac8694f9718790f
Jim3Things <56886685+Jim3Things@users.noreply.github.com>
2020-02-28 17:37:40 -0800

    Merge proto compilation in

--- build/buildall.cmd
+++ build/buildall.cmd
@@ -1,16 +1,24 @@
-pushd .
+pushd %gopath%\src
 
-cd /d %GOPATH%\src\github.com\Jim3Things\CloudChamber\cmd\controllerd
-go build -o %GOPATH%\src\github.com\Jim3Things\CloudChamber\deployments\controllerd.exe main.go
+protoc --go_out=. github.com\Jim3Things\CloudChamber\pkg\protos\common\capacity.proto
+protoc --go_out=. github.com\Jim3Things\CloudChamber\pkg\protos\common\timestamp.proto
+protoc --go_out=. github.com\Jim3Things\CloudChamber\pkg\protos\log\entry.proto
+protoc --go_out=. github.com\Jim3Things\CloudChamber\pkg\protos\inventory\actual.proto
+protoc --go_out=. github.com\Jim3Things\CloudChamber\pkg\protos\inventory\external.proto
+protoc --go_out=. github.com\Jim3Things\CloudChamber\pkg\protos\inventory\internal.proto
+protoc --go_out=. github.com\Jim3Things\CloudChamber\pkg\protos\inventory\target.proto
+protoc --go_out=. github.com\Jim3Things\CloudChamber\pkg\protos\workload\actual.proto
+protoc --go_out=. github.com\Jim3Things\CloudChamber\pkg\protos\workload\external.proto
+protoc --go_out=. github.com\Jim3Things\CloudChamber\pkg\protos\workload\internal.proto
+protoc --go_out=. github.com\Jim3Things\CloudChamber\pkg\protos\workload\target.proto
 
-cd /d %GOPATH%\src\github.com\Jim3Things\CloudChamber\cmd\inventoryd
-go build -o %GOPATH%\src\github.com\Jim3Things\CloudChamber\deployments\inventoryd.exe main.go
+protoc --go_out=plugins=grpc:. github.com\Jim3Things\CloudChamber\pkg\protos\monitor\monitor.proto
+protoc --go_out=plugins=grpc:. github.com\Jim3Things\CloudChamber\pkg\protos\Stepper\stepper.proto
 
-cd /d %GOPATH%\src\github.com\Jim3Things\CloudChamber\cmd\sim_supportd
-go build -o %GOPATH%\src\github.com\Jim3Things\CloudChamber\deployments\sim_supportd.exe main.go
-
-cd /d %GOPATH%\src\github.com\Jim3Things\CloudChamber\cmd\web_server
-go build -o %GOPATH%\src\github.com\Jim3Things\CloudChamber\deployments\web_server.exe main.go
+go build -o github.com\Jim3Things\CloudChamber\deployments\controllerd.exe github.com\Jim3Things\CloudChamber\cmd\controllerd\main.go
+go build -o github.com\Jim3Things\CloudChamber\deployments\inventoryd.exe github.com\Jim3Things\CloudChamber\cmd\inventoryd\main.go
+go build -o github.com\Jim3Things\CloudChamber\deployments\sim_supportd.exe github.com\Jim3Things\CloudChamber\cmd\sim_supportd\main.go
+go build -o github.com\Jim3Things\CloudChamber\deployments\web_server.exe github.com\Jim3Things\CloudChamber\cmd\web_server\main.go
 
 popd
 
