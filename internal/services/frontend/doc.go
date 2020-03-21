f84db26460a2db8ce1164f847d412fd4a1267ced
manpreet85 <mmal080@aucklanduni.ac.nz>
2020-03-21 16:53:09 +1300

    doc change

--- internal/services/frontend/doc.go
+++ internal/services/frontend/doc.go
@@ -101,12 +101,37 @@ Updates the workload record the for workload matching workloadname according to 
 operation, or returns and error if the supplied workloadname is not known, or the
 operation code is invalid in some way.
 
+GET - /api/racks
+Get the list of all known racks.
+
+GET - /api/racks/{rackid}
+Returns a single rack ID record.
+
+GET - /api/racks/rack-id/{blades}
+Get list of known blades in a rack.
+
+GET - /api/racks/rack-id/{TOR}
+Get Top of Racks details .
+
+GET - /api/racks/rack-id/{PDU}
+Gets Power distribution Unit details.
+
+GET - /api/racks/rack-id/blades/{blade-id}
+
+Returns a record details of a single blade.
+
+
+
 
 
 TODO
+
 /api/logs
+
 /api/stepper
 /api/injector
 /api/inventory
+will be a configuration file. you will be
+/point to cmd. Few of them per server.
 */
 package frontend
