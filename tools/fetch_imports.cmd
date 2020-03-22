782f329ace1e71dbad230c94b291d4a9b9c4339f
Jim3Things <56886685+Jim3Things@users.noreply.github.com>
2020-03-21 17:41:36 -0700

    Include all cmd files

--- tools/fetch_imports.cmd
+++ tools/fetch_imports.cmd
@@ -14,7 +14,7 @@ rem fetch & install the protobuf validation plugin
 rem
 go get -d github.com/envoyproxy/protoc-gen-validate
 pushd %GOPATH%\src\github.com\envoyproxy\protoc-gen-validate
-make build
+go install .
 popd
 
 
@@ -24,6 +24,8 @@ go get github.com/gorilla/mux
 go get github.com/gorilla/securecookie
 go get github.com/gorilla/sessions
 
+go get github.com/davecgh/go-spew/spew
+go get github.com/pmezard/go-difflib/difflib
 go get github.com/stretchr/testify/assert
 
 go get go.opentelemetry.io/otel
