4ef181afe2ab9ecf59bccd1c1c774142afaf8476
MikeCJohnsonUK <mike.c.johnson@gmail.com>
2020-04-08 15:24:34 -0700

    Add ability to run store tests using embedded etcd

--- build/run_all_uts.cmd
+++ build/run_all_uts.cmd
@@ -3,8 +3,8 @@ pushd .
 cd /d %GOPATH%\src\github.com\Jim3Things\CloudChamber\internal\clients\timestamp
 go test -v
 
-REM cd /d %GOPATH%\src\github.com\Jim3Things\CloudChamber\internal\clients\store
-REM go test -v
+cd /d %GOPATH%\src\github.com\Jim3Things\CloudChamber\internal\clients\store
+go test -v
 
 cd /d %GOPATH%\src\github.com\Jim3Things\CloudChamber\internal\services\stepper
 go test -v
