da451807d0cd5137d0b1a585fc00b48575ea0a57
MikeCJohnsonUk <58154607+MikeCJohnsonUk@users.noreply.github.com>
2020-04-21 02:17:36 -0700

    Fix compilation error, update build tools to use locally built protoc extensions and generally reduce the chattines of some of the scripts. (#23)

--- build/dev_tools/updateall.cmd
+++ build/dev_tools/updateall.cmd
@@ -2,16 +2,7 @@ pushd %GOPATH%\src
 
 go get -u github.com/golang/protobuf
 go get -u github.com/golang/protobuf/protoc-gen-go
-
-
-rem fetch & install the protobuf validation plugin
-rem
-go get -d github.com/envoyproxy/protoc-gen-validate
-pushd %GOPATH%\src\github.com\envoyproxy\protoc-gen-validate
-go install .
-popd
-
-go get -u go.etcd.io/etcd
+go get -u github.com/envoyproxy/protoc-gen-validate
 
 go get -u github.com/gorilla/mux
 go get -u github.com/gorilla/securecookie
@@ -19,14 +10,15 @@ go get -u github.com/gorilla/sessions
 
 go get -u github.com/davecgh/go-spew/spew
 go get -u github.com/pmezard/go-difflib/difflib
+go get -u github.com/spf13/viper
 go get -u github.com/stretchr/testify/assert
+
+go get -u go.etcd.io/etcd
 
 go get -u go.opentelemetry.io/otel
 
 go get -u google.golang.org/grpc
 
 go get -u golang.org/x/crypto/...
-
-go get -u github.com/spf13/viper
 
 popd
