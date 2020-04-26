b1f32f6710c3004adaadb8e218123eb9d241660d
MikeCJohnsonUk <MikeCJohnsonUk@users.noreply.github.com>
2020-04-25 19:47:55 -0700

    Respond to PR comments and fix a few typos

--- build/dev_tools/fetchall.sh
+++ build/dev_tools/fetchall.sh
@@ -10,13 +10,13 @@ go get github.com/gorilla/mux
 go get github.com/gorilla/securecookie
 go get github.com/gorilla/sessions
 
-#go get github.com/davecgh/go-spew/spew
-#go get github.com/pmezard/go-difflib/difflib
+go get github.com/davecgh/go-spew/spew
+go get github.com/pmezard/go-difflib/difflib
 go get github.com/spf13/viper
 go get github.com/stretchr/testify/assert
 
 go get go.etcd.io/etcd
-go get go.etcd.io/etcd/etdctl
+go get go.etcd.io/etcd/etcdctl
 
 go get go.opentelemetry.io/otel
 
