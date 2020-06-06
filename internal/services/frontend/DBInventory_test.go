ab98ad88824a97f7cc2413a88e68ccb5aa96a0ab
manpreet85 <mmal080@aucklanduni.ac.nz>
2020-06-06 17:13:33 +1200

    Rack list test added

--- internal/services/frontend/DBInventory_test.go
+++ internal/services/frontend/DBInventory_test.go
@@ -1,61 +1,28 @@
-package unit_test
+// Unit tests for the web service frontend.
+//
+// Borrows heavily from the gorilla mux test package.
+//
+package frontend
 
 import (
-	"context"
+	"log"
+	"net/http/httptest"
 	"testing"
 
-	export "go.opentelemetry.io/otel/sdk/export/trace"
-
-	"github.com/Jim3Things/CloudChamber/internal/tracing/exporters/common"
-	"github.com/Jim3Things/CloudChamber/pkg/protos/log"
+	"github.com/Jim3Things/CloudChamber/internal/tracing/exporters/unit_test"
 )
 
-// Options are the options to be used when initializing a unit test export.
-type Options struct {
+// // First DBInventory unit test
+func TestListRacks(t *testing.T) {
+	unit_test.SetTesting(t)
+
+	request := httptest.NewRequest("GET", "/api/racks/", nil)
+	response := doHTTP(request, nil)
+	body, err := getBody(response)
+
+	log.Printf("ListRacks: SC=%v, Content-Type='%v'\n", response.StatusCode, response.Header.Get("Content-Type"))
+	log.Println(string(body))
+	log.Printf("error: %v", err)
 }
 
-// Exporter is an implementation of trace.Exporter that writes spans to the unit test logger.
-type Exporter struct {
-}
-
-var testContext *testing.T
-
-func NewExporter(_ Options) (*Exporter, error) {
-	return &Exporter{}, nil
-}
-
-func SetTesting(item *testing.T) {
-	testContext = item
-}
-
-func (e *Exporter) ExportSpan(ctx context.Context, data *export.SpanData) {
-	entry := common.ExtractEntry(ctx, data)
-
-	testContext.Logf("[%s:%s] %s %s:\n%s", entry.GetSpanID(), entry.GetParentID(), entry.GetStatus(), entry.GetName(), entry.GetStackTrace())
-
-	for _, event := range entry.Event {
-		if event.GetTick() < 0 {
-			testContext.Logf("       : [%s] (%s) %s\n%s", severityFlag(event.GetSeverity()), event.GetName(), event.GetText(), event.GetStackTrace())
-		} else {
-			testContext.Logf("  @%4d: [%s] (%s) %s\n%s", event.GetTick(), severityFlag(event.GetSeverity()), event.GetName(), event.GetText(), event.GetStackTrace())
-		}
-	}
-}
-
-func severityFlag(severity log.Severity) string {
-	var severityToText = map[log.Severity]string {
-		log.Severity_Debug: "D",
-		log.Severity_Reason: "R",
-		log.Severity_Info: "I",
-		log.Severity_Warning: "W",
-		log.Severity_Error: "E",
-		log.Severity_Fatal: "F",
-	}
-
-	t, ok := severityToText[severity]
-	if !ok {
-		t = "X"
-	}
-
-	return t
-}
+// List racks
