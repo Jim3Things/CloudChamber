2c17e273b6a271402f322d1557d035c6d8550668
manpreet85 <mmal080@aucklanduni.ac.nz>
2020-06-13 18:21:41 +1200

    RackList test08Jun20 (#54)
    * RackList test08Jun20
    
    * Changes as per Jim's comments :DBInventory tests
    
    * Rack2 checked
    
    * DBInventory_testChanges_13Jun20

--- internal/services/frontend/DBInventory.go
+++ internal/services/frontend/DBInventory.go
@@ -10,29 +10,17 @@ package frontend
 
 import (
 	"sync"
+
+	common "github.com/Jim3Things/CloudChamber/pkg/protos/common"
+	pb "github.com/Jim3Things/CloudChamber/pkg/protos/inventory"
 )
-
-type Tor struct {
-}
-
-type Pdu struct {
-}
-
-type blade struct {
-}
-
-type rack struct {
-	tor    Tor
-	pdu    Pdu
-	blades map[int64]*blade
-}
 
 // DBInventory is a container used to establish synchronized access to
 // the in-memory set of Racks records.
-//
+
 type DBInventory struct { //A struct for the collection of Racks
 	Mutex sync.Mutex
-	Racks map[string]*rack
+	Racks map[string]*pb.ExternalRack
 }
 
 var dbInventory *DBInventory
@@ -41,26 +29,41 @@ func InitDBInventory() error {
 	if dbInventory == nil {
 		dbInventory = &DBInventory{
 			Mutex: sync.Mutex{},
-			Racks: make(map[string]*rack),
+			Racks: make(map[string]*pb.ExternalRack),
 		}
-		dbInventory.Racks["rack1"] = &rack{ //
+		dbInventory.Racks["rack1"] = &pb.ExternalRack{ //
 
-			tor:    Tor{},
-			pdu:    Pdu{},
-			blades: make(map[int64]*blade),
+			Tor:    &pb.ExternalTor{},
+			Pdu:    &pb.ExternalPdu{},
+			Blades: make(map[int64]*common.BladeCapacity),
 		}
-		dbInventory.Racks["rack2"] = &rack{ //
+		dbInventory.Racks["rack2"] = &pb.ExternalRack{ //
 
-			tor:    Tor{},
-			pdu:    Pdu{},
-			blades: make(map[int64]*blade),
+			Tor:    &pb.ExternalTor{},
+			Pdu:    &pb.ExternalPdu{},
+			Blades: make(map[int64]*common.BladeCapacity),
 		}
-		dbInventory.Racks["rack1"].blades[1] = &blade{} //First blade for rack 1.
-		dbInventory.Racks["rack1"].blades[2] = &blade{} //Second blade for rack 1.
+		dbInventory.Racks["rack1"].Blades[1] = &common.BladeCapacity{} //First blade for rack 1.
+		dbInventory.Racks["rack1"].Blades[2] = &common.BladeCapacity{} //Second blade for rack 1.
 
-		dbInventory.Racks["rack2"].blades[1] = &blade{} //First blade for rack 2.
-		dbInventory.Racks["rack2"].blades[2] = &blade{} //Second blade for rack 2.
+		dbInventory.Racks["rack2"].Blades[1] = &common.BladeCapacity{} //First blade for rack 2.
+		dbInventory.Racks["rack2"].Blades[2] = &common.BladeCapacity{} //Second blade for rack 2.
 
+	}
+
+	return nil
+}
+
+// Scan the set of known blades the store, invoking the supplied
+// function with each entry.
+func (m *DBInventory) Scan(action func(entry string) error) error {
+	m.Mutex.Lock()
+	defer m.Mutex.Unlock()
+
+	for name := range dbInventory.Racks {
+		if err := action(name); err != nil {
+			return err
+		}
 	}
 
 	return nil
--- internal/services/frontend/DBInventory_test.go
+++ internal/services/frontend/DBInventory_test.go
@@ -5,15 +5,18 @@
 package frontend
 
 import (
+	"fmt"
 	"log"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/Jim3Things/CloudChamber/internal/tracing/exporters/unit_test"
+	"github.com/stretchr/testify/assert"
 )
 
 // // First DBInventory unit test
-func TestListRacks(t *testing.T) {
+func TestInventoryListRacks(t *testing.T) {
 	unit_test.SetTesting(t)
 
 	request := httptest.NewRequest("GET", "/api/racks/", nil)
@@ -21,8 +24,14 @@ func TestListRacks(t *testing.T) {
 	body, err := getBody(response)
 
 	log.Printf("ListRacks: SC=%v, Content-Type='%v'\n", response.StatusCode, response.Header.Get("Content-Type"))
-	log.Println(string(body))
+
+	s := string(body)                   // Converted into a string
+	var splits = strings.Split(s, "\n") // Created an array per line
+	fmt.Println(splits)                 // just a print statement
+	expected := []string{"/api/racks/rack1", "/api/racks/rack2", ""}
+
+	assert.Equal(t, splits[0], "Racks (List)")
+	assert.ElementsMatch(t, expected, splits[1:])
+
 	log.Printf("error: %v", err)
 }
-
-// List racks
