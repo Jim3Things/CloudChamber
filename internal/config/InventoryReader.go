6359d4464e0e6cd5ec1bffe601bac4b438239619
manpreet85 <mmal080@aucklanduni.ac.nz>
2020-08-23 18:10:57 +1200

    TestsInventoryRead_23Aug20

--- internal/config/InventoryReader.go
+++ internal/config/InventoryReader.go
@@ -9,6 +9,7 @@ import (
 	"github.com/spf13/viper"
 	"go.opentelemetry.io/otel/api/global"
 
+	"github.com/Jim3Things/CloudChamber/pkg/protos/common"
 	pb "github.com/Jim3Things/CloudChamber/pkg/protos/inventory"
 )
 
@@ -16,49 +17,98 @@ const(
 	DefaultDefinitionFile = "inventory.yaml"
 )
 
+type zone struct {
+	Racks []rack
+}
+
+type rack struct {
+	Name string
+	Blades []blade
+	Tor tor
+	Pdu pdu
+}
+
+type blade struct {
+	Index int64
+	Arch string
+	Cores int64
+	DiskInGb int64
+	MemoryInMb int64
+	NetworkBandwidthInMbps int64
+}
+
+type tor struct {
+
+}
+
+type pdu struct {
+
+}
+
 func ReadInventoryDefinition(path string) (*pb.ExternalZone, error){
 
 	viper.SetConfigName(DefaultDefinitionFile)
 	viper.AddConfigPath(path)
 	viper.SetConfigType(DefaultConfigType)
 
-	cfg := &pb.ExternalZone{}
-
+	
 	tr := global.TraceProvider().Tracer("")
 
 	ctx, span :=tr.Start(
 			context.Background(),
 			"ReadInventoryDefinition")
-		defer span.End()
-		if err := viper.ReadInConfig(); err != nil {
-			if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-							// Config file not found; we'll just use the default values
-							span.AddEvent(
-											ctx,
-											fmt.Sprintf(
-															"No inventory definitino found at %s/%s (%s), applying defaults.",
-															path,
-															DefaultDefinitionFile,
-															DefaultConfigType))
-			} else {
-							// Config file was found but another error was produced
-							err = fmt.Errorf("fatal error reading definition file: %s", err)
-							span.AddEvent(ctx, err.Error())
-							return nil, err
-			}
-} else {
-			// Fill in the global configuration object from the configuration file
-			if err = viper.UnmarshalExact(cfg); err != nil {
-							err = fmt.Errorf("unable to decode into struct, %v", err)
-							span.AddEvent(ctx, err.Error())
-							return nil, err
-			}
+	defer span.End()
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; we'll just use the default values
+			err = fmt.Errorf("No inventory definition found at %s/%s (%s)",
+					path,
+					DefaultDefinitionFile,
+					DefaultConfigType)
+			span.AddEvent(ctx, err.Error())
+			return nil, err
+		} else {
+			// Config file was found but another error was produced
+			err = fmt.Errorf("fatal error reading definition file: %s", err)
+			span.AddEvent(ctx, err.Error())
+			return nil, err
+		}
+	} 
+
+	// Fill in the global configuration object from the configuration file
+	xfr := &zone{}
+	if err := viper.UnmarshalExact(xfr); err != nil {
+		err = fmt.Errorf("unable to decode into struct, %v", err)
+		span.AddEvent(ctx, err.Error())
+		return nil, err
+	}
+	// function to translate xfr into an external zone
+	cfg := ToExternalZone(xfr)
+	span.AddEvent(ctx,
+		fmt.Sprintf("Inventory definition Read: \n%v", cfg))
+	return cfg, nil	
 }
 
-span.AddEvent(ctx,
-			fmt.Sprintf("Inventory definition Read: \n%v", cfg))
-
-return cfg, nil
+func ToExternalZone (xfr *zone) *pb.ExternalZone{
+	cfg := &pb.ExternalZone{
+		Racks : make(map[string]*pb.ExternalRack),
+	}
+	for _, r := range xfr.Racks {
+		cfg.Racks [r.Name] = &pb.ExternalRack{
+			Tor: &pb.ExternalTor{},
+			Pdu: &pb.ExternalPdu{},
+			Blades: make(map[int64]*common.BladeCapacity),
+		}
+		for _, b := range r.Blades{
+			cfg.Racks [r.Name].Blades[b.Index] = &common.BladeCapacity{
+				Cores: b.Cores,
+				MemoryInMb: b.MemoryInMb,
+				DiskInGb: b.DiskInGb,
+				NetworkBandwidthInMbps: b.NetworkBandwidthInMbps,
+				Arch: b.Arch,
+			}
+		}
+	}
+ return cfg
 }
-
-
--- internal/config/InventoryReader_test.go
+++ internal/config/InventoryReader_test.go
@@ -2,18 +2,54 @@
 package config
 
 import (
+	"os"
 	"testing"
 
+	"github.com/Jim3Things/CloudChamber/internal/tracing/exporters"
+	"github.com/Jim3Things/CloudChamber/internal/tracing/exporters/unit_test"
+	"github.com/Jim3Things/CloudChamber/internal/tracing/setup"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+// Common test startup method.  This is the _only_ Test* function in this
+// file.
+func TestMain(m *testing.M) {
+	commonSetup()
+
+	os.Exit(m.Run())
+}
+
+// Establish the test environment, including starting a test frontend service
+// over a faked http connection.
+func commonSetup() {
+	setup.Init(exporters.UnitTest)
+}
 
 // first inventory definition test
 
-func ReadInventoryDefinitiontest (t *testing.T) {
+func TestReadInventoryDefinition (t *testing.T) {
 
-	response := ReadInventoryDefinition("C:\\Users\\Waheguru")
+	unit_test.SetTesting(t)
+	defer unit_test.SetTesting(nil)
 
-	 assert.Equal(t, "C:\\User\\Waheguru", response)
-	 
+	response, err := ReadInventoryDefinition(".")
+	require.Nil(t, err)
+
+	require.Equal(t, 2, len(response.Racks))
+
+
+	r,ok:= response.Racks["rack1"]
+	require.True(t, ok)
+	assert.Equal(t, 2, len(r.Blades))
+	
+	b,ok:=r.Blades[1]
+	require.True(t, ok)
+	assert.Equal(t, int64(16), b.Cores )
+	
+
+
+		
 }
+
 
