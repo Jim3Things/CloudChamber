455c384a9744a82afe6d1d0613c8e37aec75b094
Jim3Things <56886685+Jim3Things@users.noreply.github.com>
2020-08-25 08:31:51 -0700

    Stepper bugfix, minor cleanup

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -6,6 +6,8 @@ package version
 import (
 	"context"
 	"fmt"
+	"os"
+	"time"
 
 	"go.opentelemetry.io/otel/api/global"
 	"go.opentelemetry.io/otel/api/trace"
@@ -13,7 +15,7 @@ import (
 
 //go:generate go run generator/generate.go
 
-// ToString is a function which returns the version information formatted as a
+// toString is a function which returns the version information formatted as a
 // single line, suitable for dumping to a log file.
 //
 func toString() string {
@@ -28,7 +30,7 @@ func toString() string {
 }
 
 // Show is a function which prints the version information formatted as a
-// multiple line set of values to Stdout, suiltable for responding to a
+// multiple line set of values to Stdout, suitable for responding to a
 // version option flag
 //
 func Show() {
@@ -42,14 +44,21 @@ func Show() {
 		BuildBranchHash)
 }
 
-// TraceVersion is a simple function to insert a trace record containing the
-// version information into the trace stream.
+// Trace is a simple function to insert the leading trace events that show the
+// startup of the service, and the version information associated with it.
 //
 func Trace() {
 	tr := global.TraceProvider().Tracer("")
 
 	_ = tr.WithSpan(context.Background(), "TraceVersion", func(ctx context.Context) (err error) {
 		span := trace.SpanFromContext(ctx)
+
+		span.AddEvent(
+			ctx,
+			fmt.Sprintf(
+				"===== Starting %q at %s =====",
+				fmt.Sprint(os.Args),
+				time.Now().Format(time.RFC1123Z)))
 		span.AddEvent(ctx, toString())
 		return nil
 	})
