7c1645a16a32e466fa79fe2d1bdd4b12f2ae9327
Jim3Things <56886685+Jim3Things@users.noreply.github.com>
2020-08-25 11:06:10 -0700

    Create skeleton handler modules
    Create the skeleton logs and injection handlers to avoid single-threading on frontend.go

---
+++ internal/services/frontend/injection.go
@@ -0,0 +1,22 @@
+package frontend
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func injectionAddRoutes(routeBase *mux.Router) {
+	router := routeBase.PathPrefix("/inject").Subrouter()
+
+	router.HandleFunc("", handlerInjectionRoot).Methods("GET")
+	router.HandleFunc("/", handlerInjectionRoot).Methods("GET")
+}
+
+func handlerInjectionRoot(w http.ResponseWriter, _ *http.Request) {
+
+	_, _ = fmt.Fprintf(w, "Injection (Root)")
+}
+
+
