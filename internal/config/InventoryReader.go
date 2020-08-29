b2e867323d9d2970cce8ede8d3cd08767e5b6d27
manpreet85 <mmal080@aucklanduni.ac.nz>
2020-08-29 18:44:21 +1200

    Comments for InventoryReaderFile-29Aug20
    Formatting and comments changed

--- internal/config/InventoryReader.go
+++ internal/config/InventoryReader.go
@@ -1,6 +1,6 @@
-// Inventory reader parses the YAML file and returns Zone. into a pb external zone.
-
 package config
+
+// Inventory reader parses the YAML file and returns Zone. into a pb external zone.
 
 import (
 	"context"
@@ -14,17 +14,18 @@ import (
 )
 
 const(
-	DefaultDefinitionFile = "inventory.yaml"
+	defaultDefinitionFile = "inventory.yaml"
 )
 
-//The local variables below were created to meet the requirements that would enable us to read the YAML file
-//zone is a local struct with a field as a map of Racks
+// +++ Intermediate binary format
+
+// This mirrors the external zone format except the keys
+// are fields of array
 
 type zone struct {
 	Racks []rack
 }
 
-//Similarly rack itself is a map of Blades, map of Tor and a map of Pdu
 type rack struct {
 	Name string
 	Blades []blade
@@ -32,7 +33,6 @@ type rack struct {
 	Pdu pdu
 }
 
-//Finally blade is the most basic struct in a zone with key value pairs as listed below
 type blade struct {
 	Index int64
 	Arch string
@@ -50,13 +50,17 @@ type pdu struct {
 
 }
 
+// --- Intermediate binary format
+
+// ReadInventoryDefinition imports the inventory from 
+// external YAML file and transforms it into the
+// internal Cloud chamber binary format.
 func ReadInventoryDefinition(path string) (*pb.ExternalZone, error){
 
-	viper.SetConfigName(DefaultDefinitionFile)
+	viper.SetConfigName(defaultDefinitionFile)
 	viper.AddConfigPath(path)
 	viper.SetConfigType(DefaultConfigType)
 
-	
 	tr := global.TraceProvider().Tracer("")
 
 	ctx, span :=tr.Start(
@@ -66,49 +70,52 @@ func ReadInventoryDefinition(path string) (*pb.ExternalZone, error){
 
 	if err := viper.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			// Config file not found; we'll just use the default values
 			err = fmt.Errorf("No inventory definition found at %s/%s (%s)",
 					path,
-					DefaultDefinitionFile,
+					defaultDefinitionFile,
 					DefaultConfigType)
-			span.AddEvent(ctx, err.Error())
-			return nil, err
 		} else {
-			// Config file was found but another error was produced
 			err = fmt.Errorf("fatal error reading definition file: %s", err)
-			span.AddEvent(ctx, err.Error())
-			return nil, err
 		}
+
+		span.AddEvent(ctx, err.Error())
+		return nil, err
 	} 
 
-	// Fill in the global configuration object from the configuration file
+	// First we are going to put it into intermediate format 
 	xfr := &zone{}
 	if err := viper.UnmarshalExact(xfr); err != nil {
 		err = fmt.Errorf("unable to decode into struct, %v", err)
 		span.AddEvent(ctx, err.Error())
 		return nil, err
 	}
-	// function to translate xfr into an external zone
-	cfg := ToExternalZone(xfr)
+
+	// Now convert it into its final form
+	cfg := toExternalZone(xfr)
+
 	span.AddEvent(ctx,
 		fmt.Sprintf("Inventory definition Read: \n%v", cfg))
 	return cfg, nil	
 }
 
-// Creating new function to convert array values into YAML readable maps
-
-func ToExternalZone (xfr *zone) *pb.ExternalZone{
-	cfg := &pb.ExternalZone{ //Capturing values from pb.externalZone into a new variable
+// toExternalZone converts intermediate values to the final format
+// One important differnce is that the intermediate is array based.
+// The final format is map based using specific fields in array 
+// enteries as the map keys
+func toExternalZone(xfr *zone) *pb.ExternalZone{
+	cfg := &pb.ExternalZone{ 
 		Racks : make(map[string]*pb.ExternalRack),
 	}
-	for _, r := range xfr.Racks { // Loop through all the racks and captures info about Tor, Pdu and blades for that rack
+
+	for _, r := range xfr.Racks { 
 		cfg.Racks [r.Name] = &pb.ExternalRack{
 			Tor: &pb.ExternalTor{},
 			Pdu: &pb.ExternalPdu{},
 			Blades: make(map[int64]*common.BladeCapacity),
 		}
-		for _, b := range r.Blades{//loop through each blade in a rack and capture infor in variable b about each key in that blade
-			cfg.Racks [r.Name].Blades[b.Index] = &common.BladeCapacity{//Pointer to &common.BladeCapacity is the value of Blade Index for a specific blade
+
+		for _, b := range r.Blades{
+			cfg.Racks [r.Name].Blades[b.Index] = &common.BladeCapacity{
 				Cores: b.Cores,
 				MemoryInMb: b.MemoryInMb,
 				DiskInGb: b.DiskInGb,
@@ -117,5 +124,6 @@ func ToExternalZone (xfr *zone) *pb.ExternalZone{
 			}
 		}
 	}
- return cfg // return the values into the cfg variable 
+
+ 	return cfg 
 }
