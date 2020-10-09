009c3591cf463927c153a6dd84b43cc8a5a37f8d
Jim3Things <56886685+Jim3Things@users.noreply.github.com>
2020-10-09 13:45:08 -0700

    Update inventory.go

--- internal/services/inventory/inventory.go
+++ internal/services/inventory/inventory.go
@@ -6,27 +6,24 @@ import (
 	"google.golang.org/grpc"
 
 	"github.com/Jim3Things/CloudChamber/internal/config"
+	"github.com/Jim3Things/CloudChamber/internal/tracing"
 	pb "github.com/Jim3Things/CloudChamber/pkg/protos/services"
 )
 
 type server struct {
 	pb.UnimplementedInventoryServer
 
-	racks map[string]rack
+	racks map[string]*rack
 }
 
 func Register(svc *grpc.Server, cfg *config.GlobalConfig) error {
 	s := &server {
-
+		racks: make(map[string]*rack),
 	}
 
-	// Read the configuration to get the inventory link
-
-	// Read the inventory
-
-	// For each rack, create a rack item, supplying the tor, pdu, and blade
-
-	// Start each rack (this gives us a channel and a goroutine)
+	if err := s.initializeRacks(cfg.Inventory.InventoryDefinition); err != nil {
+		return err
+	}
 
 	pb.RegisterInventoryServer(svc, s)
 
@@ -49,3 +46,25 @@ func (s *server) GetCurrentStatus(context.Context, *pb.InventoryStatusMsg) (*pb.
 	return nil, nil
 }
 
+func (s *server) initializeRacks(path string) error {
+	ctx, span := tracing.StartSpan(context.Background(), tracing.WithName("Initializing the simulated inventory"))
+	defer span.End()
+
+	zone, err := config.ReadInventoryDefinition(path)
+	if err != nil {
+		return err
+	}
+
+	for name, r := range zone.Racks {
+		// For each rack, create a rack item, supplying the tor, pdu, and blade
+		tracing.Info(ctx, "Adding rack %q", name)
+		s.racks[name] = newRack(ctx, r)
+
+		// Start each rack (this gives us a channel and a goroutine)
+		if err = s.racks[name].start(ctx); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
