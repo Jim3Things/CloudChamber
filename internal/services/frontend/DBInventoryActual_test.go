d80064e416f98a6490dfbb15e2a823dab1006903
manpreet85 <mmal080@aucklanduni.ac.nz>
2020-10-11 17:50:28 +1300

    Inventory Compared to Actual-tests

--- internal/services/frontend/DBInventoryActual_test.go
+++ internal/services/frontend/DBInventoryActual_test.go
@@ -1,16 +1,35 @@
-package main
+package frontend
 
 import (
 	"github.com/stretchr/testify/require"
 	"testing"
-)
+	"github.com/stretchr/testify/assert"
+	)
 
 func TestInitDBInventoryActual(t *testing.T){
 
-	response, err := InitDBInventoryActual(inven *DBInventory)
-	require.NotNil(t, err)
-	r, ok := actual.Zone.Racks["rack1"]
-	require.True(t, ok)
-	assert.Equal(t, 8, len(r.Blades))
+ 	err := InitDBInventoryActual(dbInventory)
+	require.Nil(t, err)
 
+	assert.Equal(t, len(dbInventory.Zone.Racks), len(dbInventoryActual.Zone.Racks))
+
+	for name, rack := range dbInventory.Zone.Racks{
+		r, ok := dbInventoryActual.Zone.Racks[name]
+		require.True(t, ok)
+		
+		assert.Equal(t, len(rack.Blades), len(r.Blades))
+		for bladeID := range rack.Blades{
+			b, ok := r.Blades[bladeID]
+			require.True(t, ok)
+			assert.Equal(t, bladeWorking, b.State)
+		}
+		
+		assert.Equal(t, rackWorking, r.State)
+		assert.Equal(t, torWorking, r.Tor.State)
+		assert.Equal(t, pduWorking, r.Pdu.State)
+
+	}
+			
 }
+
+ 
