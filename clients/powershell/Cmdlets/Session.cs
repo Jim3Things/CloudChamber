659650a8bf0ce946ae0133e1f9295ed310533e5a
Jim3Things <56886685+Jim3Things@users.noreply.github.com>
2021-02-02 13:24:00 -0800

    Merge powerhsell cmdlet project into cloudchamber repo (#207)

---
+++ clients/powershell/Cmdlets/Session.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Management.Automation;
+using System.Net.Http;
+
+namespace CloudChamber.Cmdlets
+{
+    /// <summary>
+    /// Session holds the state for an active connection to a cluster.
+    /// </summary>
+    public class Session : IDisposable
+    {
+        /// <summary>
+        /// Client is the active HTTP client to use for any REST calls.
+        /// </summary>
+        public HttpClient Client { get; private set; }
+
+        /// <summary>
+        /// Name is the username for the currently logged in user.
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// This constructor initializes the session and establishes the client
+        /// context for the target cluster.
+        /// </summary>
+        /// <param name="uri">address of the target cluster.</param>
+        /// <param name="name">logged in name.</param>
+        public Session(Uri uri, string name)
+        {
+            Name = name;
+            Client = new HttpClient
+            {
+                BaseAddress = uri
+            };
+        }
+
+        /// <summary>
+        /// Performs the tasks associated with freeing, releasing, or resetting
+        /// the http client resources.
+        /// </summary>
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+
+            if (Client != null)
+            {
+                Client.Dispose();
+                Client = null;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Connect to a specified account on the target cluster.
+    /// </summary>
+    [Cmdlet(VerbsCommunications.Connect, Names.Account)]
+    public class LoginCmdlet : Cmdlet
+    {
+        /// <summary>
+        /// ClusterUri is the address of the target cluster.
+        /// </summary>
+        [Parameter(Position = 0, Mandatory = true)]
+        public Uri ClusterUri { get; set; }
+
+        /// <summary>
+        /// Name is the username of the account to log in.
+        /// </summary>
+        [Parameter(Position = 1, Mandatory = true)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Password is the password string to use when logging in.
+        /// </summary>
+        [Parameter(Position = 2, Mandatory = true)]
+        public string Password { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            var session = new Session(ClusterUri, Name);
+            var path = $"/api/users/{Name}?op=login";
+
+            var resp = session.Client.PutAsync(path, new StringContent(Password)).Result;
+
+            resp.EnsureSuccessStatusCode();
+
+            var msg = resp.Content.ReadAsStringAsync().Result;
+
+            WriteObject(session);
+        }
+    }
+
+    /// <summary>
+    /// Disconnect an active session.
+    /// </summary>
+    [Cmdlet(VerbsCommunications.Disconnect, Names.Account)]
+    public class LogoutCmdlet : Cmdlet
+    {
+        /// <summary>
+        /// Session is the active session to disconnect.
+        /// </summary>
+        [Parameter(Mandatory = true)]
+        public Session Session { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            var path = $"/api/users/{Session.Name}?op=logout";
+
+            var resp = Session.Client.PutAsync(path, new StringContent(string.Empty)).Result;
+
+            resp.EnsureSuccessStatusCode();
+
+            var msg = resp.Content.ReadAsStringAsync().Result;
+
+            WriteObject(Session);
+        }
+    }
+}
---
+++ clients/powershell/Cmdlets/protos/JsonConverters.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace CloudChamber.Cmdlets.Protos
+{
+    /// <summary>
+    ///     DurationConverter converts between the JSON representation of a protobuf
+    ///     Duration type and a C# TimeSpan.
+    /// </summary>
+    public class DurationConverter : JsonConverter<TimeSpan>
+    {
+        public override bool CanRead => true;
+        public override bool CanWrite => true;
+
+        /// <summary>
+        ///     Format a TimeSpan value into the duration JSON string
+        /// </summary>
+        /// <param name="writer">output JSON writer</param>
+        /// <param name="value">TimeSpan to record</param>
+        /// <param name="serializer">unused</param>
+        public override void WriteJson(JsonWriter writer, TimeSpan value, JsonSerializer serializer)
+        {
+            var sec = value.TotalMilliseconds / 1000;
+            var s = $"{sec}s";
+            writer.WriteValue(s);
+        }
+
+        /// <summary>
+        ///     Convert a JSON string of the form {ss}.{ff}s into a TimeSpan.
+        /// </summary>
+        /// <param name="reader">JSON data reader</param>
+        /// <param name="objectType">unused</param>
+        /// <param name="existingValue">unused</param>
+        /// <param name="hasExistingValue">unused</param>
+        /// <param name="serializer">unused</param>
+        /// <returns>Corresponding TimeSpan value</returns>
+        public override TimeSpan ReadJson(
+            JsonReader reader,
+            Type objectType,
+            TimeSpan existingValue,
+            bool hasExistingValue,
+            JsonSerializer serializer)
+        {
+            var s = (string) reader.Value;
+            if (string.IsNullOrEmpty(s)) return TimeSpan.Zero;
+
+            if (!float.TryParse(s.Trim().TrimEnd('s'), out var f)) return TimeSpan.Zero;
+
+            var ms = (int) (f * 1_000);
+            return new TimeSpan(0, 0, 0, 0, ms);
+        }
+    }
+}
