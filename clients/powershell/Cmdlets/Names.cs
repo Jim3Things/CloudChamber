1ccf1a58ca7dd4b224af67e3476719e23fb14ed8
Jim3Things <56886685+Jim3Things@users.noreply.github.com>
2021-02-02 10:10:01 -0800

    Merge powerhsell cmdlet project into cloudchamber repo

---
+++ clients/powershell/Cmdlets/Names.cs
@@ -0,0 +1,17 @@
+﻿namespace CloudChamber.Cmdlets
+{
+    /// <summary>
+    /// Names is the collection of Powershell object names used by this cmdlet
+    /// module.
+    /// </summary>
+    internal static class Names
+    {
+        internal const string Users = "CcUsers";
+        internal const string User = "CcUser";
+
+        internal const string Account = "CcAccount";
+
+        internal const string Time = "CcTime";
+        internal const string TimePolicy = "CcTimePolicy";
+    }
+}
