f3b1a32da075b5ebf7a6a96c2caf4fd18b39d4b0
Mike Johnson <58154607+MikeCJohnsonUk@users.noreply.github.com>
2021-02-15 15:16:16 -0800

    Convert  inventory definition tests to be based on the testify test suite package (#219)
    * Convert  inventory definition tests to testify suite based
    
    Change the definition tests to be based on the testify package. Requires moving to a common core test suite to allow each package to (reader, definition) to make use of the same common core. Note flag package must only parse flags exactly once.
    
    * Updates resulting from PR comments
    
    Co-authored-by: MikeCJohnsonUK <MikeCJohnsonUk@users.noreply.github.com>

---
+++ simulation/internal/clients/inventory/TestSuiteCore.go
@@ -0,0 +1,80 @@
+package inventory
+
+import (
+	"flag"
+	"sync"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/Jim3Things/CloudChamber/simulation/internal/clients/store"
+	"github.com/Jim3Things/CloudChamber/simulation/internal/config"
+	"github.com/Jim3Things/CloudChamber/simulation/internal/tracing/exporters"
+)
+
+var (
+	lock         sync.Mutex
+	globalCfg    *config.GlobalConfig
+	initialized  bool = false
+)
+
+type testSuiteCore struct {
+	suite.Suite
+
+	baseURI string
+
+	utf *exporters.Exporter
+
+	cfg *config.GlobalConfig
+
+	store *store.Store
+}
+
+func runOnce() (*config.GlobalConfig, error) {
+	lock.Lock()
+	defer lock.Unlock()
+
+	if !initialized {
+		configPath := flag.String("config", "./testdata", "path to the configuration file")
+
+		flag.Parse()
+
+		cfg, err := config.ReadGlobalConfig(*configPath)
+		if err != nil {
+			return nil, err
+		}
+
+		globalCfg = cfg
+		initialized = true
+	}
+
+	return globalCfg, nil
+}
+
+func (ts *testSuiteCore) SetupSuite() {
+	require := ts.Require()
+
+	cfg, err := runOnce()
+	require.NoError(err, "failed to process the global configuration")
+
+	ts.utf = exporters.NewExporter(exporters.NewUTForwarder())
+	exporters.ConnectToProvider(ts.utf)
+
+	store.Initialize(cfg)
+
+	ts.cfg = cfg
+	ts.store = store.NewStore()
+}
+
+func (ts *testSuiteCore) SetupTest() {
+	require := ts.Require()
+
+	require.NoError(ts.utf.Open(ts.T()))
+	require.NoError(ts.store.Connect())
+}
+
+func (ts *testSuiteCore) TearDownTest() {
+	ts.store.Disconnect()
+	ts.utf.Close()
+}
+
+
