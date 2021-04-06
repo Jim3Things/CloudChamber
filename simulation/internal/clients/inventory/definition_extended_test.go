e59408a18d6957fabe3dbdde601a3fd7da2b9e6e
MikeCJohnsonUK <MikeCJohnsonUk@users.noreply.github.com>
2021-04-06 15:09:32 -0700

    Remove some dead code

--- simulation/internal/clients/inventory/definition_extended_test.go
+++ simulation/internal/clients/inventory/definition_extended_test.go
@@ -11,50 +11,12 @@ type definitionExtendedTestSuite struct {
 	testSuiteCore
 
 	inventory      *Inventory
-
-	// regionCount    int
-	// zonesPerRegion int
-	// racksPerZone   int
-	// pdusPerRack    int
-	// torsPerRack    int
-	// bladesPerRack  int
-
-	// portsPerPdu int
-	// portsPerTor int
 }
 
 func (ts *definitionExtendedTestSuite) SetupSuite() {
-	// require := ts.Require()
-
-	// ctx := context.Background()
-
 	ts.testSuiteCore.SetupSuite()
 
 	ts.inventory = NewInventory(ts.cfg, ts.store)
-
-	// // These values are relatively arbitrary. The only criteria is that different
-	// // constants were chosen to help separate different multiples of different
-	// // object types where possible and not to have values which are too large
-	// // to avoid lots of IO when setting up the test suite.
-	// //
-	// ts.regionCount = 2
-	// ts.zonesPerRegion = 3
-	// ts.racksPerZone = 4
-	// ts.pdusPerRack = 1
-	// ts.torsPerRack = 1
-	// ts.bladesPerRack = 5
-
-	// ts.portsPerPdu = ts.torsPerRack + ts.bladesPerRack
-	// ts.portsPerTor = ts.pdusPerRack + ts.torsPerRack + ts.bladesPerRack
-
-	// require.NoError(ts.utf.Open(ts.T()))
-	// require.NoError(ts.store.Connect())
-
-	// err := ts.createStandardInventory(ctx)
-	// require.NoError(err, "failed to create standard inventory")
-
-	// ts.store.Disconnect()
-	// ts.utf.Close()
 }
 
 func (ts *definitionExtendedTestSuite) SetupTest() {
